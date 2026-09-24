6528be7d79dd8892ae6f1a0a3ec51154f0064293
Arsyi Qurrota Aini <arsyiaini@gmail.com>
2026-09-24 12:29:30 +0700

    fix error api auth

--- lib/features/authentication/services/auth_service.dart
+++ lib/features/authentication/services/auth_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
-  static const String baseUrl = 'https://bakso-angkringan-web-production-p47hnh.laravel.cloud';
+  static const String baseUrl = 'https://bakso-angkringan-web-production-p47hnh.laravel.cloud/api';
 
   Future<Map<String, dynamic>> login({
     required String noHp,
